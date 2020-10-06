a3a8d25c80f54e5afaa102c0bc488246d0c3bbcf
Sidmar <sidkairos@gmail.com>
2020-10-06 09:50:58 -0300

    Revert "Criada a classe Empréstimo"
    This reverts commit 2481899a969f6ea0f1d7f87e0764ff98c8c15cfc.

--- src/biblioteca/BibliotecaApp.java
+++ src/biblioteca/BibliotecaApp.java
@@ -3,11 +3,7 @@ package biblioteca;
 public class BibliotecaApp {
 
 	public static void main(String[] args) {
-		
 		Livro l = new Livro();
-		Usuario u = new Usuario();
-		
-		Emprestimo emprestimo = new Emprestimo();
 	}
 
 }
